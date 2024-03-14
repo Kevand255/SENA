25399d6835a22df10f1087839db00b9aacca59a0
Kevand255 <keviinandres255@gmail.com>
2024-03-14 15:12:27 -0500

    ciclos

---
+++ Diana/ciclos/cicloDoWhile.js
@@ -0,0 +1,5 @@
+let numero = 200
+do {
+    console.log(numero)
+    numero+=4   
+} while (numero<401);
--- Diana/pasajeros/pasajeros.js
+++ Diana/pasajeros/pasajeros.js
@@ -8,29 +8,40 @@ console.log(`Primer parada:
 Pasajeros bajados: 0
 Pasajeros subidos: ${pasajerosSubidos}`);
 
-let pasajerosBajadosParada2 = getRandomIntInclusive(0, pasajerosSubidos);
-let asientosLibresParada2 = asientosLibres + pasajerosBajadosParada2
-let pasajerosSubidosParada2 =   getRandomIntInclusive(0, asientosLibresParada2)
-let pasajerosAbordo = pasajerosSubidosParada2 + pasajerosSubidos - pasajerosBajadosParada2
-console.log(`Segunda parada: 
-Pasajeros bajados: ${pasajerosBajadosParada2}
-Pasajeros subidos: ${pasajerosSubidosParada2}`)
 
 
-let pasajerosBajadosParada3 = getRandomIntInclusive(0, pasajerosAbordo)
-let asientosLibresParada3 = (asientosLibresParada2 - pasajerosSubidosParada2) + pasajerosBajadosParada3
-let pasajerosSubidosParada3 = getRandomIntInclusive(0, asientosLibresParada3)
-console.log(`Tercer parada: 
-Pasajeros bajados: ${pasajerosBajadosParada3}
-Pasajeros subidos: ${pasajerosSubidosParada3}`)
+
+// let pasajerosBajadosParada2 = getRandomIntInclusive(0, pasajerosSubidos);
+// let asientosLibresParada2 = asientosLibres + pasajerosBajadosParada2
+// let pasajerosSubidosParada2 =   getRandomIntInclusive(0, asientosLibresParada2)
+// let pasajerosAbordo = pasajerosSubidosParada2 + pasajerosSubidos - pasajerosBajadosParada2
+// console.log(`Segunda parada: 
+// Pasajeros bajados: ${pasajerosBajadosParada2}
+// Pasajeros subidos: ${pasajerosSubidosParada2}`)
 
 
-let pasajerosBajadosParada4 = getRandomIntInclusive(1,pasajerosSubidosParada3)
-let asientosLibresParada4 = asientosLibresParada3 + pasajerosBajadosParada4
-let pasajerosSubidosParada4 = getRandomIntInclusive(1,asientosLibresParada4)
-console.log(`Parada cuatro:
-Pasajeros bajados: ${pasajerosBajadosParada4}
-Pasajeros subidos: ${pasajerosSubidosParada4}`)
+
+
+// let pasajerosBajadosParada3 = getRandomIntInclusive(0, pasajerosAbordo)
+// let asientosLibresParada3 = (asientosLibresParada2 - pasajerosSubidosParada2) + pasajerosBajadosParada3
+// let pasajerosSubidosParada3 = getRandomIntInclusive(0, asientosLibresParada3)
+// console.log(`Tercer parada: 
+// Pasajeros bajados: ${pasajerosBajadosParada3}
+// Pasajeros subidos: ${pasajerosSubidosParada3}`)
+
+
+
+
+// let pasajerosBajadosParada4 = getRandomIntInclusive(1,pasajerosSubidosParada3)
+// let asientosLibresParada4 = asientosLibresParada3 + pasajerosBajadosParada4
+// let pasajerosSubidosParada4 = getRandomIntInclusive(1,asientosLibresParada4)
+// console.log(`Parada cuatro:
+// Pasajeros bajados: ${pasajerosBajadosParada4}
+// Pasajeros subidos: ${pasajerosSubidosParada4}`)
+
+
+
+
 // EN LA PARADA 5 TODOS SE TIENEN QUE BAJAR
 
 // let pasajerosBajadosParada5 = getRandomIntInclusive(1,pasajerosSubidosParada4)
@@ -39,6 +50,8 @@ Pasajeros subidos: ${pasajerosSubidosParada4}`)
 // console.log(`Parada cinco:
 // Pasajeros bajados: ${pasajerosBajadosParada5}
 // Pasajeros subidos: ${pasajerosSubidosParada5}`)
+
+
 
 // let totalPasajereros = pasajerosSubidos + pasajerosSubidos   Parada2 + pasajerosSubidosParada3+pasajerosSubidosParada4+pasajerosSubidosParada5
 // let dineroEnCaja = totalPasajereros*3000
